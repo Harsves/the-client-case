c6f9ab6acc45f40663da7ced297760586c5e27a9
Suuskehh <suus.harsveld@hva.nl>
2022-12-06 11:35:33 +0100

    Scroll to op

--- script.js
+++ script.js
@@ -15,12 +15,15 @@ window.onclick = function(event) {
   }
 }
 
-// const toTop = document.querySelector(".to-top");
+const toTop = document.querySelector(".to-top");
 
-// window.addEventListener("scroll", () => {
-//   if (window.pageYOffset > 100) {
-//     toTop.classList.add("active");
-//   } else {
-//     toTop.classList.remove("active");
-//   }
-// })
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("to-top-active");
+  } else {
+    toTop.classList.remove("to-top-active");
+  }
+})
+
+function topFunction() { window.scrollTo(0, 0); 
+}
