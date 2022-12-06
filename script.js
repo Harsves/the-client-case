4ec56bb500c0bd9c133148484c72e5b2524517de
Suuskehh <suus.harsveld@hva.nl>
2022-12-06 12:08:16 +0100

    Dropdown

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-function myFunction() {
-  document.getElementById("dropdown").classList.toggle("show");
-}
+const list = document.querySelector('.dropdown-content')
+const btn = document.querySelector('.dropdown-nav')
 
-window.onclick = function(event) {
-  if (!event.target.matches('.dropdown-nav')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+btn.addEventListener('click', (e)=> {
+  
+ list.classList.toggle('show')
+  e.stopPropagation()
+})
+
+document.addEventListener('click', (e)=> {
+  if(e.target.closest('.dropdown-content')) return 
+  
+  list.classList.remove('show')
+
+})
 
 const toTop = document.querySelector(".to-top");
 
